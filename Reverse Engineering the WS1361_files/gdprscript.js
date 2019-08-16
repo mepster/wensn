ad636c72cb973b71c8f79f28d73064f0cd557afb
Michael E. Palmer <mep@mepalmer.net>
2019-08-16 16:08:22 -0700

    remove files from ebswift's web site

--- Reverse Engineering the WS1361_files/gdprscript.js
+++
@@ -1,4 +0,0 @@
-// Script created for GDPR Compliance. Source code located: weebly/kings-banner
-
-document.cookie = "gdpr-kb=true; expires=Thu, 01 Jan 2999 12:00:00 GMT; path=/";
-window.AragornAnalytics && window.AragornAnalytics.initialize();
